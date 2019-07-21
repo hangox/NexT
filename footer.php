f628fb88e044f27081e6416df7f387cd20cbdd3b
HangoX <liang.hanguang93@gmail.com>
2019-07-21 16:45:56 +0800

    增加更多的代码样式

--- footer.php
+++ footer.php
@@ -1,25 +1,28 @@
-    <footer id="footer" class="footer">
-        <div class="footer-inner">
-            <div class="copyright">
-                <?php
-                    $next_site_year = get_option('next_site_year');
-                    $current_year = date(Y);
-                    if ($next_site_year >= $current_year) {
-                        $output_year = "<span>".$current_year."</span>";
-                    }
-                    else{
-                        $output_year = "<span>".$next_site_year."</span> - "."<span>".$current_year."</span>";
-                    }
-                ?>
-                &copy; <?php echo $output_year; ?> <span class="with-love"> <i class="icon-next-heart fa fa-heart"></i></span>
-                <span class="author"><?php $username = get_option('next_author','admin'); echo get_profile( 'display_name', $username ); ?></span>
-            </div>
-            <div class="powered-by">Powered by <a href="//cn.wordpress.org" target="_blank">WordPress</a></div>
-            <div class="theme-info">Theme <a href="https://github.com/pengtikui/NexT" target="_blank">NexT WP</a></div>
-            <div style="display:none"><?php echo get_option('next_analyze_code'); ?></div>
+<footer id="footer" class="footer">
+    <div class="footer-inner">
+        <div class="copyright">
+            <?php
+            $next_site_year = get_option('next_site_year');
+            $current_year = date(Y);
+            if ($next_site_year >= $current_year) {
+                $output_year = "<span>" . $current_year . "</span>";
+            } else {
+                $output_year = "<span>" . $next_site_year . "</span> - " . "<span>" . $current_year . "</span>";
+            }
+            ?>
+            &copy; <?php echo $output_year; ?> <span class="with-love"> <i
+                        class="icon-next-heart fa fa-heart"></i></span>
+            <span class="author"><?php $username = get_option('next_author', 'admin');
+                echo get_profile('display_name', $username); ?></span>
         </div>
-    </footer>
-    <div class="back-to-top"><i class="fa fa-arrow-up"></i></div>
+        <div class="powered-by">Powered by <a href="//cn.wordpress.org"
+                                              target="_blank">WordPress</a></div>
+        <div class="theme-info">Theme <a href="https://github.com/pengtikui/NexT" target="_blank">NexT
+                WP</a></div>
+        <div style="display:none"><?php echo get_option('next_analyze_code'); ?></div>
+    </div>
+</footer>
+<div class="back-to-top"><i class="fa fa-arrow-up"></i></div>
 </div>
 <script type="text/javascript">
     if (Object.prototype.toString.call(window.Promise) !== '[object Function]') {
@@ -35,13 +38,13 @@
 <script src="<?php bloginfo('template_url'); ?>/statics/js/utils.js"></script>
 <script src="<?php bloginfo('template_url'); ?>/statics/js/motion.js"></script>
 <script src="<?php bloginfo('template_url'); ?>/statics/js/affix.js"></script>
-<?php if ( get_option('next_scheme', 'muse') == 'pisces' ) { ?>
-<script src="<?php bloginfo('template_url'); ?>/statics/js/pisces.js"></script>
+<?php if (get_option('next_scheme', 'muse') == 'pisces') { ?>
+    <script src="<?php bloginfo('template_url'); ?>/statics/js/pisces.js"></script>
 <?php } ?>
 
-<?php if ( is_single() || is_page() ) { ?>
-<script src="<?php bloginfo('template_url'); ?>/statics/js/scrollspy.js"></script>
-<script src="<?php bloginfo('template_url'); ?>/statics/js/post-details.js"></script>
+<?php if (is_single() || is_page()) { ?>
+    <script src="<?php bloginfo('template_url'); ?>/statics/js/scrollspy.js"></script>
+    <script src="<?php bloginfo('template_url'); ?>/statics/js/post-details.js"></script>
 <?php } ?>
 
 <!-- 归档页 -->
@@ -51,9 +54,23 @@
 
 <script src="<?php bloginfo('template_url'); ?>/statics/js/bootstrap.js"></script>
 <!-- highlight.js -->
-    <script src="//cdn.bootcss.com/highlight.js/9.15.8/highlight.min.js"></script>
+<script src="//cdn.bootcss.com/highlight.js/9.15.8/highlight.min.js"></script>
+<script src="https://cdn.bootcss.com/highlight.js/9.15.8/languages/groovy.min.js"></script>
+<script src="https://cdn.bootcss.com/highlight.js/9.15.8/languages/yaml.min.js"></script>
+<script src="https://cdn.bootcss.com/highlight.js/9.15.8/languages/dockerfile.min.js"></script>
+<script src="https://cdn.bootcss.com/highlight.js/9.15.8/languages/dart.min.js"></script>
+<script src="https://cdn.bootcss.com/highlight.js/9.15.8/languages/go.min.js"></script>
+<script src="https://cdn.bootcss.com/highlight.js/9.15.8/languages/json.min.js"></script>
+<script src="https://cdn.bootcss.com/highlight.js/9.15.8/languages/gradle.min.js"></script>
+<script src="https://cdn.bootcss.com/highlight.js/9.15.8/languages/css.min.js"></script>
+<script src="https://cdn.bootcss.com/highlight.js/9.15.8/languages/aspectj.min.js"></script>
+<script src="https://cdn.bootcss.com/highlight.js/9.15.8/languages/kotlin.min.js"></script>
+<script src="https://cdn.bootcss.com/highlight.js/9.15.8/languages/markdown.min.js"></script>
+<script src="https://cdn.bootcss.com/highlight.js/9.15.8/languages/makefile.min.js"></script>
+<script src="https://cdn.bootcss.com/highlight.js/9.15.8/languages/nginx.min.js"></script>
+<script src="https://cdn.bootcss.com/highlight.js/9.15.8/languages/powershell.min.js"></script>
 <script>
-    $('pre code').each(function(i, block) {
+    $('pre code').each(function (i, block) {
         hljs.highlightBlock(block);
     });
 </script>
